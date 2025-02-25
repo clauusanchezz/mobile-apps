99e48b52a87d90d4b92495201d0e526c4bedbe83
Claudia Sanchez <claudiasanchez@MacBook-Air-de-Claudia.local>
2025-02-25 13:54:05 +0100

    prueba 1 github

---
+++ app/src/main/java/com/uc3m/android/helloworld/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.uc3m.android.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.uc3m.android.helloworld.ui.theme.HelloWorldTheme
+import androidx.compose.ui.res.stringResource
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(R.string.hello, name),
+        )
+        Button(onClick = {
+            Log.d("MainActivity", "Button clicked!")
+        }) {
+            Text("Click Me!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("Android")
+    }
+}
